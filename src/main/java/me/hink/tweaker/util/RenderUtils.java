3dab0d0d111b734aefd6d8226e8bfa34841f002f
h1nk <h1nk@users.noreply.github.com>
2017-11-02 05:33:12 -0400

    added stuff to RenderUtils for rendering the debug bounding boxes

--- src/main/java/me/hink/tweaker/util/RenderUtils.java
+++ src/main/java/me/hink/tweaker/util/RenderUtils.java
@@ -28,18 +28,13 @@ import net.minecraftforge.client.event.DrawBlockHighlightEvent;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.WorldRenderer;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
+
+import static net.minecraft.client.renderer.RenderGlobal.drawSelectionBoundingBox;
 
 /**
  * TODO
@@ -48,58 +43,89 @@ import net.minecraft.util.Vec3;
  * @since	0.0.1
  */
 public class RenderUtils {
-	public static EntityRenderer getDefaultEntityRenderer() {
-		return Minecraft.getMinecraft().entityRenderer;
-	}
-
-	public Vec3 getIncorrectLookVector(float partialTicks) {
-		EntityPlayerSP thePlayer = Minecraft.getMinecraft().thePlayer;
-
-		if (thePlayer != null) {
-			if (partialTicks == 1.0F) {
-				return thePlayer.getVectorForRotation(thePlayer.rotationPitch, thePlayer.rotationYaw);
-			} else {
-				float f1 = thePlayer.prevRotationPitch + (thePlayer.rotationPitch - thePlayer.prevRotationPitch) * partialTicks;
-				float f2 = thePlayer.prevRotationYawHead + (thePlayer.rotationYawHead - thePlayer.prevRotationYawHead) * partialTicks;
-
-				return thePlayer.getVectorForRotation(f1, f2);
-			}
-		} else return null;
-	}
+//	public static EntityRenderer getDefaultEntityRenderer() {
+//		return Minecraft.getMinecraft().entityRenderer; // FIXME
+//	}
 
 	public static void drawCustomSelectionBox(DrawBlockHighlightEvent event) {
-//		drawSelectionBox(event.player, event.target, event.subID, event.partialTicks, 2.0f);
+//		drawDefaultSelectionBox(event.player, event.target, event.subID, event.partialTicks); // TODO
+//		event.context.drawSelectionBox(event.player, event.context.mc.objectMouseOver, 0, event.partialTicks);
 	}
 
-	public static void drawDebugSelectionBox(DrawBlockHighlightEvent event) {
-		drawSelectionBox(event.player, event.target, event.subID, event.partialTicks, 0.0f);
+	public static void drawCorrectedSelectionBox(DrawBlockHighlightEvent event) {
+		drawCustomSelectionBox(event.player, event.target, event.subID, event.partialTicks, 0.0f, 1.0f);
 	}
 
-	public static void drawSelectionBox(EntityPlayer player, MovingObjectPosition movingObjectPositionIn, int execute, float partialTicks, float color) {
-		Minecraft mc = Minecraft.getMinecraft();
+	public static void drawIncorrectSelectionBox(DrawBlockHighlightEvent event) {
+		MovingObjectPosition latentPosition = latentYawRayTrace(event.player, 5.0, event.partialTicks);
 
+		drawCustomSelectionBox(event.player, latentPosition, event.subID, event.partialTicks, 1.0f, 0.0f);
+	}
+
+	/**
+	 * Draws the selection box for the player. Args: entityPlayer, rayTraceHit, i, itemStack, partialTickTime
+	 *
+	 * @param execute If equals to 0 the method is executed
+	 */
+	public static void drawCustomizedSelectionBox(EntityPlayer player, MovingObjectPosition movingObjectPositionIn, int execute, float partialTicks) {
+		// TODO
+	}
+
+	/**
+	 *
+	 * @param entity
+	 * @param blockReachDistance
+	 * @param partialTicks
+	 * @return
+	 */
+	public static MovingObjectPosition latentYawRayTrace(EntityPlayer entity, double blockReachDistance, float partialTicks) {
+		Vec3 vec31;
+
+		Vec3 vec3 = entity.getPositionEyes(partialTicks);
+
+		// Vec3 vec31 = this.getLook(partialTicks);
+		if (partialTicks == 1.0F) {
+			vec31 = entity.getVectorForRotation(entity.rotationPitch, entity.rotationYawHead);
+		} else {
+			float f = entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * partialTicks;
+			float f1 = entity.prevRotationYawHead + (entity.rotationYawHead - entity.prevRotationYawHead) * partialTicks;
+			vec31 = entity.getVectorForRotation(f, f1);
+		}
+
+		Vec3 vec32 = vec3.addVector(vec31.xCoord * blockReachDistance, vec31.yCoord * blockReachDistance, vec31.zCoord * blockReachDistance);
+
+		return entity.worldObj.rayTraceBlocks(vec3, vec32, false, false, true);
+	}
+
+	/**
+	 * Draws the selection box for the player. Args: entityPlayer, rayTraceHit, i, itemStack, partialTickTime
+	 *
+	 * @param execute If equals to 0 the method is executed
+	 */
+//	public static void drawDefaultSelectionBox(EntityPlayer player, MovingObjectPosition movingObjectPositionIn, int execute, float partialTicks) {
+	private static void drawDefaultSelectionBox(EntityPlayer player, MovingObjectPosition movingObjectPositionIn, int execute, float partialTicks) {
 		if (execute == 0 && movingObjectPositionIn.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
 			GlStateManager.enableBlend();
 			GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-			GlStateManager.color(0.0f, 1.0f, 0.0f, 0.75f);
-
-			GL11.glLineWidth(7.5F);
-
+			GlStateManager.color(0.0F, 0.0F, 0.0F, 0.4F);
+			GL11.glLineWidth(2.0F);
 			GlStateManager.disableTexture2D();
 			GlStateManager.depthMask(false);
-
+			float f = 0.002F; // FIXME
 			BlockPos blockpos = movingObjectPositionIn.getBlockPos();
+//			Block block = this.theWorld.getBlockState(blockpos).getBlock();
+			Block block = player.worldObj.getBlockState(blockpos).getBlock();
 
-			Block block = mc.theWorld.getBlockState(blockpos).getBlock();
-
-			if (block.getMaterial() != Material.air && mc.theWorld.getWorldBorder().contains(blockpos)) {
-				block.setBlockBoundsBasedOnState(mc.theWorld, blockpos);
-
+//			if (block.getMaterial() != Material.air && this.theWorld.getWorldBorder().contains(blockpos))
+			if (block.getMaterial() != Material.air && player.worldObj.getWorldBorder().contains(blockpos))
+			{
+//				block.setBlockBoundsBasedOnState(this.theWorld, blockpos);
+				block.setBlockBoundsBasedOnState(player.worldObj, blockpos);
 				double d0 = player.lastTickPosX + (player.posX - player.lastTickPosX) * (double)partialTicks;
 				double d1 = player.lastTickPosY + (player.posY - player.lastTickPosY) * (double)partialTicks;
 				double d2 = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * (double)partialTicks;
-
-				drawSelectionBoundingBox(block.getSelectedBoundingBox(mc.theWorld, blockpos).expand(0.0020000000949949026D, 0.0020000000949949026D, 0.0020000000949949026D).offset(-d0, -d1, -d2));
+//				drawSelectionBoundingBox(block.getSelectedBoundingBox(this.theWorld, blockpos).expand(0.0020000000949949026D, 0.0020000000949949026D, 0.0020000000949949026D).offset(-d0, -d1, -d2));
+				drawSelectionBoundingBox(block.getSelectedBoundingBox(player.worldObj, blockpos).expand(0.0020000000949949026D, 0.0020000000949949026D, 0.0020000000949949026D).offset(-d0, -d1, -d2));
 			}
 
 			GlStateManager.depthMask(true);
@@ -108,35 +134,28 @@ public class RenderUtils {
 		}
 	}
 
-	private static void drawSelectionBoundingBox(AxisAlignedBB boundingBox) {
-		Tessellator tessellator = Tessellator.getInstance();
-		WorldRenderer worldRenderer = tessellator.getWorldRenderer();
+	private static void drawCustomSelectionBox(EntityPlayer player, MovingObjectPosition movingObjectPositionIn, int execute, float partialTicks, float red, float green) {
+		if (execute == 0 && movingObjectPositionIn.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
+			GlStateManager.enableBlend();
+			GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+			GlStateManager.color(red, green, 0.0F, 0.7F);
+			GL11.glLineWidth(5.0F);
+			GlStateManager.disableTexture2D();
+			GlStateManager.depthMask(false);
+			BlockPos blockpos = movingObjectPositionIn.getBlockPos();
+			Block block = player.worldObj.getBlockState(blockpos).getBlock();
 
-		worldRenderer.begin(3, DefaultVertexFormats.POSITION);
-		worldRenderer.pos(boundingBox.minX, boundingBox.minY, boundingBox.minZ).endVertex();
-		worldRenderer.pos(boundingBox.maxX, boundingBox.minY, boundingBox.minZ).endVertex();
-		worldRenderer.pos(boundingBox.maxX, boundingBox.minY, boundingBox.maxZ).endVertex();
-		worldRenderer.pos(boundingBox.minX, boundingBox.minY, boundingBox.maxZ).endVertex();
-		worldRenderer.pos(boundingBox.minX, boundingBox.minY, boundingBox.minZ).endVertex();
-		tessellator.draw();
+			if (block.getMaterial() != Material.air && player.worldObj.getWorldBorder().contains(blockpos)) {
+				block.setBlockBoundsBasedOnState(player.worldObj, blockpos);
+				double d0 = player.lastTickPosX + (player.posX - player.lastTickPosX) * (double)partialTicks;
+				double d1 = player.lastTickPosY + (player.posY - player.lastTickPosY) * (double)partialTicks;
+				double d2 = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * (double)partialTicks;
+				drawSelectionBoundingBox(block.getSelectedBoundingBox(player.worldObj, blockpos).expand(0.0020000000949949026D, 0.0020000000949949026D, 0.0020000000949949026D).offset(-d0, -d1, -d2));
+			}
 
-		worldRenderer.begin(3, DefaultVertexFormats.POSITION);
-		worldRenderer.pos(boundingBox.minX, boundingBox.maxY, boundingBox.minZ).endVertex();
-		worldRenderer.pos(boundingBox.maxX, boundingBox.maxY, boundingBox.minZ).endVertex();
-		worldRenderer.pos(boundingBox.maxX, boundingBox.maxY, boundingBox.maxZ).endVertex();
-		worldRenderer.pos(boundingBox.minX, boundingBox.maxY, boundingBox.maxZ).endVertex();
-		worldRenderer.pos(boundingBox.minX, boundingBox.maxY, boundingBox.minZ).endVertex();
-		tessellator.draw();
-
-		worldRenderer.begin(1, DefaultVertexFormats.POSITION);
-		worldRenderer.pos(boundingBox.minX, boundingBox.minY, boundingBox.minZ).endVertex();
-		worldRenderer.pos(boundingBox.minX, boundingBox.maxY, boundingBox.minZ).endVertex();
-		worldRenderer.pos(boundingBox.maxX, boundingBox.minY, boundingBox.minZ).endVertex();
-		worldRenderer.pos(boundingBox.maxX, boundingBox.maxY, boundingBox.minZ).endVertex();
-		worldRenderer.pos(boundingBox.maxX, boundingBox.minY, boundingBox.maxZ).endVertex();
-		worldRenderer.pos(boundingBox.maxX, boundingBox.maxY, boundingBox.maxZ).endVertex();
-		worldRenderer.pos(boundingBox.minX, boundingBox.minY, boundingBox.maxZ).endVertex();
-		worldRenderer.pos(boundingBox.minX, boundingBox.maxY, boundingBox.maxZ).endVertex();
-		tessellator.draw();
+			GlStateManager.depthMask(true);
+			GlStateManager.enableTexture2D();
+			GlStateManager.disableBlend();
+		}
 	}
 }
